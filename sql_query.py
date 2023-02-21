1b0abd35430777c1fe71badfb9be70ff2a2bdd6e
OleksandrCherniavskyi <105165580+OleksandrCherniavskyi@users.noreply.github.com>
2023-02-21 19:24:59 +0100

    Create sql_query.py

---
+++ sql_query.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+# Create a SQL connection to our SQLite database
+con = sqlite3.connect("justjoin.sqlite")
+
+cur = con.cursor()
+
+# The result of a "cursor.execute" can be iterated over by row
+for row in cur.execute("""
+SELECT SUM(Count) AS TotalCount
+FROM (
+  SELECT COUNT(*) AS Count
+  FROM tech
+  GROUP BY Technologies
+) subquery;
+"""):
+    print(row)
+
+# Be sure to close the connection
+con.close()
+
+
+#Count Technologies
+SELECT SUM(Count) AS TotalCount
+FROM (
+  SELECT COUNT(*) AS Count
+  FROM tech
+  GROUP BY Technologies
+) subquery;
